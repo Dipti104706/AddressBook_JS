ced579134976c431e0ff2df3d0bec3bf19536712
Dipti104706 <diptimayeebehura104@gmail.com>
2021-10-27 17:40:45 +0530

    [Dipti] added code for modify existing contact details in address book

--- AddressBook.js
+++ AddressBook.js
@@ -1,7 +1,19 @@
 var prompt=require("prompt-sync")();
-//UC3 - Adding contact in a array
+//UC4 - Edit existing contact 
 class Person
 {
+    constructor(...params)
+    {
+        this.firstName=params[0];
+        this.lastName=params[1];
+        this.address=params[2];
+        this.city=params[3];
+        this.state=params[4];
+        this.pin=params[5];
+        this.phone=params[6];
+        this.emailID=params[7];
+    }
+    
     //Getter setter method
     get firstName()
     {
@@ -87,7 +99,7 @@ class Person
     }
     set phone(value)
     {
-        let phoneRegex = RegExp("^[1-9]{2}+[ ]+[0-9]{10}$");
+        let phoneRegex = RegExp("^[0-9]+\\s[0-9]{10}$");
         if(phoneRegex.test(value))
         this._phone = value;
         else
@@ -109,23 +121,19 @@ class Person
     //tostring method for displaying method
     toString()
     {
-        return `First name:${firstName} \nLast name:${lastName} \nAddress: ${address} \nCity:${city} \nState:${state} \nPincode:${pin} \nPhone Number:${phone} \nEmail Id: ${emailId}`;
+        return `First name:${this.firstName} \nLast name:${this.lastName} \nAddress: ${this.address} \nCity:${this.city} \nState:${this.state} \nPincode:${this.pin} \nPhone Number:${this.phone} \nEmail Id: ${this.emailID}`;
     }
 }
 
-//creating a new array
-var contactArray=new Array();
-//calling createContact
-createContact(contactArray);
-
 //function to create contact 
-function createContact(contactArray)
+function AddContact(array)
 {
     try
     {
-        var contact=parseInt(prompt("enter the number of contacts to be added: "));
-        while(contact--)
+        var contact=parseInt(prompt("Enter the number of contacts to be added: "));
+        while(contact>0)
         {
+            console.log("Add details for contact-"+contact);
             //getting the input from user 
             firstName = prompt('Enter First name : ');
             lastName = prompt('Enter Last name : ');
@@ -134,11 +142,11 @@ function createContact(contactArray)
             state = prompt('Enter State name : ');
             pin = prompt('Enter PinCode: ')
             phone = prompt('Enter Phone number :');
-            emailId= prompt('Enter EmailId : ');
+            emailID= prompt('Enter EmailId : ');
             //creating a object 
-            let contact1=new Person(firstName,lastName,address,city,state,pin,phone,emailId);
-            contactArray.push(contact1);
-            displayArray(contact1);
+            let contact1=new Person(firstName,lastName,address,city,state,pin,phone,emailID)
+            array.push(contact1);    
+            contact--; 
         }
     }
     catch(ex)
@@ -147,11 +155,112 @@ function createContact(contactArray)
     }
 }
 
+
 //Displaying all contact stored in array
-function displayArray(contact)
+function DisplayArray(contact)
 {
     for(let i=0;i<contact.length;i++)
     {
         console.log(contact[i].toString()+"\n");
     }
 }
+
+//Uc4-Modify cintact details based on firstname
+function EditContact(contact)
+ {
+    try 
+    {
+        if (contact.length > 0) 
+        {
+            var name = prompt("Enter the first name whose details want to edit:");
+            var result = contact.find(x => x.firstName == name);
+            if (result != null) 
+            {
+                while (true) 
+                {
+                    console.log("Enter the option to modify the details: \n 1-Change First name \n 2-Change Last name \n 3-Change Address \n 4-Change City \n 5-Change State \n 6-Change Pincode \n 7-Change Phone Number\n 8-Change emailID \n 9-Exit");
+                    let option = prompt("Enter the option:")
+                    switch (option) 
+                    {
+                        case "1":
+                            result.firstName = prompt("Enter the new First Name: ");
+                            break;
+                        case "2":
+                            result.lastName = prompt("Enter new last Name: ");
+                            break;
+                        case "3":
+                            result.address = prompt("Enter new address: ");
+                            break;
+                        case "4":
+                            result.city = prompt("Enter new City Name: ");
+                            break;
+                        case "5":
+                            result.state = prompt("Enter new State Name: ");
+                            break;
+                        case "6":
+                            result.pin = prompt("Enter new pincode: ");
+                            break;
+                        case "7":
+                            result.phone = prompt("Enter new Phone Number: ");
+                            break;
+                        case "8":
+                            result.emailID = prompt("Enter new Email ID: ");
+                            break;
+                        case "9":
+                            return;
+                        default:
+                            console.log("Invalid input")
+                            break;                        
+                    }
+                }
+            }
+            else 
+            {
+                console.log("Contact doesnot Exist");
+            }
+        }
+        else
+        {
+            console.log("AddressBook is null");
+        }
+    }
+
+    catch (ex) 
+    {
+        console.error(ex);
+    }
+}
+
+//Function for all opeartions like add,edit contact to the adddressbook 
+function operation()
+{
+    console.log("Welcome to Addressbook")
+    //creating a array
+    var contactArray=new Array();
+    while(true)
+    {
+        console.log("Enter the operation you want to perform: \n1-Add details to addressbook \n2-Display all contacts \n3-Modify existing contact \n4-Exit");
+        var op=prompt("Enter the option:")
+        switch(op)
+        {
+            case "1":
+                AddContact(contactArray);
+                break;
+            case "2":
+                DisplayArray(contactArray);
+                break;
+            case "3":
+                EditContact(contactArray);
+                break;
+            case"4":
+                return;
+            default:
+                console.log("Invalid option!");
+                break;
+        }
+    }
+}
+operation();
+
+
+
